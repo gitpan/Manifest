8d3181b1a00c45994e27629bbaa8cb03110b7d53
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
1997-11-23 00:32:05 +0000

    Import of JPRIT/Manifest-1.04 from CPAN.
    gitpan-cpan-distribution: Manifest
    gitpan-cpan-version:      1.04
    gitpan-cpan-path:         JPRIT/Manifest-1.04.tar.gz
    gitpan-cpan-author:       JPRIT
    gitpan-cpan-maturity:     released

--- Manifest.pm
+++ Manifest.pm
@@ -1,140 +1,225 @@
-package File::Manifest;
 use strict;
+
+=head1 NAME
+
+File::Manifest - objects representing trees of files
+
+=head1 SYNOPSIS
+
+  my $m = new File::Manifest($dir);
+  $m->write();
+
+  my $old = File::Manifest->load($d1);
+  my $new = File::Manifest->new($d2);
+  $old->diff($new);
+
+=head1 DESCRIPTION
+
+Not yet.
+
+=cut
+
+package File::Manifest;
 use Carp;
 use IO::File;
 use File::Recurse;
-use vars qw($VERSION);
+use vars qw($VERSION $DEFAULT_CKSUM);
 
-$VERSION = '1.02';
+$VERSION = '1.04';
+
+$DEFAULT_CKSUM = 'Checksum::Simple';
+
+# unfactor ? XXX
+sub FILE() { 'file' }
+sub ECONF() { 'MANICKSUM' }
+
+sub require_pm {
+  my $class = shift;
+  my $file = $class;
+  $file =~ s|::|/|g;
+  $file .= ".pm";
+  eval { require $file };
+  if ($@ and $@ !~ m"Can't locate .*? in \@INC") { die $@ }
+}
+
+sub mode {
+  my ($o, $mode) = @_;
+  $o->{mode} = $mode if defined $mode;
+  $o->{mode}
+}
+
+sub cksums {
+  my $o = shift;
+  my @boxes;
+  push(@boxes, $DEFAULT_CKSUM->new());
+  push(@boxes, $o->{cksum}) if $o->{cksum};
+  @boxes;
+}
+
+sub get_default_cksum {
+  my ($o) = @_;
+  my $cksum = $ENV{ &ECONF };
+  if ($cksum) {
+    require_pm($cksum);
+    $o->{cksum} = $cksum->new();
+  }
+}
 
 sub new {
-    my $o = bless { manifest=>'MANIFEST', cksum => {}, mode=>0644 }, shift;
+  my ($class, $dir) = @_;
+  -d $dir or croak "$class->new: $dir is not a directory";
+  my $o = bless { dir => $dir, 'mode' =>0644 }, $class;
+  $o->get_default_cksum;
+  $o;
+}
 
-    if (@_ == 1 or @_ == 2) {
-	$o->bind(@_);
-    } elsif (@_ != 0) {
-	croak "new File::Manifest([dir,dest])";
+sub load {
+  my ($class, $f) = @_;
+  if (-f $f) {
+    my $fh = new IO::File;
+    $fh->open($f) or die "open $f: $!";
+    my $h = <$fh>;
+    chomp $h;
+    my @cols = split($;, $h);
+    my $line=1;
+    my @files;
+    while (defined(my $l = <$fh>)) {
+      ++$line;
+      chomp $l;
+      my @f = split($;, $l);
+      if (@f != @cols) { warn "Field mismatch file $f line $l\n"; next }
+      
+      my %z;
+      for (my $c=0; $c < @cols; $c++) { $z{ $cols[$c] } = $f[$c]; }
+      push(@files, \%z);
     }
-    $o;
+    @files = sort { $a->{&FILE} cmp $b->{&FILE} } @files;
+    bless { files => \@files }, $class
+  } elsif (-d $f) {
+    if (-f "$f/".'MANIFEST') { $class->load("$f/".'MANIFEST'); }
+    else { $class->new($f)->reread_tree(); }
+  } else {
+    croak "$class->load: can't read $f";
+  }
 }
 
-sub add_cksum {
-    my ($o, $pkg) = @_;
-    croak "$o->add_cksum(package)" if @_ != 2;
-    return if $pkg eq 'nm';
-    $o->{cksum}{$pkg} = $pkg->new();
-}
-
-sub bind {
-    my ($o, $dir, $dest) = @_;
-    $o->{dir} = $dir;
-    $o->{dest} = $dest || $dir;
-}
-
-sub read_tree {
+sub reread_tree {
     my ($o) = @_;
-
-    chdir $o->{dir} or die "chdir $o->{dir}: $!";
+    croak "$o->reread_tree: no directory" if !$o->{dir};
+    my @boxes = $o->cksums;
+    my $fh = new IO::File;
     my @files;
     recurse(sub {
-	my $f = substr(shift, 2);
-	return 0 if (-d $f or $f eq $o->{manifest});
-	my $z = {nm=>$f};
-	while (my ($k,$box) = each %{$o->{cksum}}) {
-	    # assume all cksums have the same interface as MD5...
+	my $f = shift;
+	my $nm = substr($f, 1+length $o->{dir});
+	return 0 if (-d $f or $f =~ m,/MANIFEST$,);
+	my $z = {&FILE=>$nm};
+	for my $box (@boxes) {
 	    $box->reset();
 	    if (-l $f) { 
 		# do something reasonable with symlinks
 		$box->add(readlink($f));
 	    } else {
-		my $fh = new IO::File;
-		if (!$fh->open($f)) {
-		    warn "open $f: $!";
-		} else {
-		    $box->addfile($fh);
-		}
+		if (!$fh->open($f)) { warn "open $f: $!"; } 
+		else { $box->addfile($fh); $fh->close(); }
 	    }
-	    $z->{$k} = $box->hexdigest();
+	    $z->{ ref($box) } = $box->hexdigest();
 	}
 	push(@files, $z);
 	0;
-    }, '.');
-    @files = sort { $a->{nm} cmp $b->{nm} } @files;
-    \@files;
+    }, $o->{dir});
+    @files = sort { $a->{&FILE} cmp $b->{&FILE} } @files;
+    $o->{files} = \@files;
+    $o;
 }
     
-sub find_cksum {
-    my ($o, $sums) = @_;
-    for my $p (@$sums) { return if exists $o->{cksum}{$p} }
-    my $ok=0;
-    my @sums = @$sums;
-    while (!$ok) {
-	my $p = shift @sums;
-	die "Basic checksum not found (tried ".join(',', @$sums).")" if !$p;
-	next if $p eq 'nm';
-	my $f = "$p.pm";
-	$f =~ s,::,/,g;
-	eval { require $f; $o->add_cksum($p); $ok=1; };
-    }
-}
-
 sub write {
-    my $o = shift;
-    # bind @_?
+    my ($o, $to) = @_;
 
-    $o->find_cksum([qw(MD5 Checksum::cksum Checksum::sum)]);
+    $o->reread_tree() if !$o->{files};
 
-    my $files = $o->read_tree;
+    if (!$to) {
+      $to = new IO::File;
+      my $f = "$o->{dir}/".'MANIFEST';
+      $to->open($f, O_WRONLY|O_CREAT|O_TRUNC, $o->{'mode'}) 
+	or die "open $f: $!";
+    }
 
-    my $f = "$o->{dest}/$o->{manifest}";
-    my $fh = new IO::File;
-    $fh->open($f, O_WRONLY|O_CREAT|O_TRUNC, $o->{mode}) or die "open $f: $!";
-    my @cols = ('nm', keys %{$o->{cksum}});
-    print $fh join($;, @cols)."\n";
-    for my $f (@$files) {
-	print $fh join($;, map { $f->{$_} } @cols)."\n";
+    my @cols = (&FILE, map { ref } $o->cksums);
+    $to->print(join($;, @cols)."\n");
+    for my $f (@{$o->{files}}) {
+      $to->print(join($;, map { $f->{$_} } @cols)."\n");
     }
 }
 
 sub diff {
-    my $self = shift;
+    my ($old, $new) = @_;
+    $old->{files} or croak "Old $old is empty";
+    $new->{files} or croak "New $new is empty";
 
-    my $orig;
-    {
-	my $fh = new IO::File;
-	my $f = "$self->{dest}/$self->{manifest}";
-	$fh->open($f) or die "open $f: $!";
-	my $h = <$fh>;
-	chomp $h;
-	my @cols = split($;, $h);
-	$self->find_cksum(\@cols);
-	while (defined (my $l = <$fh>)) {
-	    chomp $l;
-	    my @z = split($;, $l);
-	    my %z;
-	    for (my $c=0; $c < @cols; $c++) { $z{$cols[$c]} = $z[$c]; }
-	    $orig->{$z{nm}} = \%z;
-	}
-    };
+    my $r;
+    for (qw(= + - !)) { $r->{$_} = [] }
 
-    my $r = { '=' => [], '+' => [], '-' => [], '!' => [] };
-    my $new = $self->read_tree;
-    for my $f (@$new) {
-	my $o = $orig->{$f->{nm}};
-	delete $orig->{$f->{nm}};  #check once only
+    my $hash;
+    for my $z (@{$old->{files}}) { $hash->{ $z->{&FILE} } = $z; }
+
+    for my $f (@{$new->{files}}) {
+	my $o = $hash->{ $f->{&FILE} };
+	delete $hash->{ $f->{&FILE} };  #check once only
 	if ($o) {
 	    my $eq=1;
 	    for my $k (keys %$f) {
 		do {$eq=0; last} if (exists $o->{$k} and $o->{$k} ne $f->{$k});
 	    }
-	    push(@{$r->{$eq? '=':'!'}}, $f);
+	    push(@{$r->{ $eq? '=':'!' }}, $f);
 	} else {
 	    push(@{$r->{'+'}}, $f);
 	}
     }
-    for my $o (values %$orig) { push(@{$r->{'-'}}, $o) }
-
+    for my $o (values %$hash) { push(@{$r->{'-'}}, $o) }
     $r;
+}
+
+package Checksum::Simple;
+use IO::File;
+
+# This is similar to Solaris 2.5.1 /usr/bin/sum, but doesn't match
+# exactly.  Hopefully it portable, since it's all written in perl.
+
+sub new {
+  my ($class) = @_;
+  my $o = bless { sum => 0 }, $class;
+  $o;
+}
+
+sub reset {
+  my ($o) = @_;
+  $o->{sum} = 0;
+}
+
+sub add {
+  use integer;
+  my $o = shift;
+  for my $l (@_) {
+    $o->{sum} += unpack("%16C*", $l);
+    $o->{sum} %= 65536;
+  }
+}
+
+sub addfile {
+  my ($o, $fh) = @_;
+  my $data;
+  while (1) {
+    my $st = sysread($fh, $data, 1024);
+    die $! if !defined $st;
+    last if $st == 0;
+    $o->add($data);
+  }
+}
+
+sub hexdigest {
+  my $o = shift;
+  $o->{sum};
 }
 
 1;
--- t/manifest.t
+++ t/manifest.t
@@ -1,30 +1,17 @@
 #-*-perl-*-
-BEGIN { $| = 1; $tx=1; print "1..9\n"; }
+BEGIN { $| = 1; $tx=1; print "1..4\n"; }
 END {print "not ok 1\n" unless $loaded;}
 
-use File::Manifest;
+use IO::File;
 $loaded=1;
 
 sub ok { print "ok $tx\n"; $tx++; }
 sub not_ok { print "not ok $tx\n"; $tx++; }
 
-system("$^X -Mblib -w manifest t/1 t 2>/dev/null")==0 or die 1;
+my $fh = new IO::File;
+$fh->open("$^X -Mblib -w ./manifest -d t/1 t/2 2>/dev/null | sort |") or die 1;
 
-my $m = new File::Manifest('t/2', 't');
-my $r = $m->diff;
-for my $c (qw(+ - ! =)) {
-    for my $f (@{$r->{$c}}) {
-	my $n = $f->{nm};
-	if (($n =~ m'plus' && $c eq '+') or
-	    ($n =~ m'minus' && $c eq '-') or
-	    ($n =~ m'(eq|ne.link)' && $c eq '=') or
-	    ($n =~ m/ne$/ && $c eq '!')) {
-	    ok;
-	} else {
-	    warn "$c $n";
-	    not_ok;
-	}
-    }
-}
-
-ok;
+<$fh> =~ m'\! ne' ? ok:not_ok;
+<$fh> =~ m'\+ plus' ? ok:not_ok;
+<$fh> =~ m'\- minus' ? ok:not_ok;
+<$fh> ? not_ok:ok;
